324e169720c3c5c89838ff526d21f5958a6a7415
Irene <choireneh@gmail.com>
2022-09-22 13:54:26 -0400

    added cat function

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pytest
 
+CAT_COLORS = ["orange", "tabby", "black", "white", ,"gray", "calico"]
 
 def always_returns_true():
     print("I love my cat Percy!")
     print("Percy is soft and fluffy")
     return True
 
+def kitty_power():
+    for cat in CAT_COLORS:
+        print(cat)
 
 def test_always_returns_true():
     assert always_returns_true()
