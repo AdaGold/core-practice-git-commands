7eada0a69dd686ef943f4e14dec6d764e1c50bf3
Anika Stephen Wilbur <anika.m.stephen@gmail.com>
2022-09-22 10:53:31 -0700

    Change multiple lines test

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ import pytest
 def always_returns_true():
     print("I love my cat Percy!")
     print("Percy is soft and fluffy")
+    
+    
+    
+    print("How old is Percy?")
+    print("I wonder if Percy would be friends with my cat Bodhi!")
+    
+    
     return True
 
 
 def test_always_returns_true():
     assert always_returns_true()
+
+
+
+    print("Surprise! I changed this line!")
